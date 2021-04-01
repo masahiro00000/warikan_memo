bd7b577516a87bd3eac207522bd6301654f00f69
Masahiro <masamasa656@gmail.com>
2021-04-01 17:01:09 +0000

    APIキーの必須化に対応

--- src/app/warikan010/warikan010.service.ts
+++ src/app/warikan010/warikan010.service.ts
@@ -3,11 +3,12 @@ import { IPayment, IRecode, IUpdateDAO } from "./warikan010.d";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    "Content-Type": "application/json",
-  }),
-};
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     "Content-Type": "application/json",
+//     "x-api-key":
+//   }),
+// };
 
 @Injectable({
   providedIn: "root",
@@ -15,23 +16,38 @@ const httpOptions = {
 export class Warikan010Service {
   constructor(private http: HttpClient) {}
 
-  url_insert =
-    "https://i74ehdz3ij.execute-api.ap-northeast-1.amazonaws.com/Prod/insert";
-
-  url_fetch =
-    "https://i74ehdz3ij.execute-api.ap-northeast-1.amazonaws.com/Prod/fetch";
-
-  url_update =
-    "https://i74ehdz3ij.execute-api.ap-northeast-1.amazonaws.com/Prod/update";
-
+  //本番用API
   // url_insert =
-  //   "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/prod/insert";
+  //   "https://i74ehdz3ij.execute-api.ap-northeast-1.amazonaws.com/Prod/insert";
 
   // url_fetch =
-  //   "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/prod/fetch";
+  //   "https://i74ehdz3ij.execute-api.ap-northeast-1.amazonaws.com/Prod/fetch";
 
   // url_update =
-  //   "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/prod/update";
+  //   "https://i74ehdz3ij.execute-api.ap-northeast-1.amazonaws.com/Prod/update";
+
+  //開発用API
+  url_insert =
+    "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/prod/insert";
+  // "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/beta/insert";
+
+  url_fetch =
+    "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/prod/fetch";
+  // "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/beta/fetch";
+
+  url_update =
+    "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/prod/update";
+  // "https://k2e5uqqls3.execute-api.ap-northeast-1.amazonaws.com/beta/update";
+
+  // 開発用APIキー
+  apiKeyDev = "R4dDdxMzY03y7dLNruv3S26C30KcurFE5qkgHgcn";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+      "x-api-key": this.apiKeyDev,
+    }),
+  };
 
   insertRecode(
     key: string,
@@ -52,14 +68,14 @@ export class Warikan010Service {
       version: version,
     };
 
-    return this.http.post(this.url_insert, body, httpOptions);
+    return this.http.post(this.url_insert, body, this.httpOptions);
   }
 
   fetchRecode(key: string) {
     let body = {
       key: key,
     };
-    return this.http.post(this.url_fetch, body, httpOptions);
+    return this.http.post(this.url_fetch, body, this.httpOptions);
   }
 
   updateRecode(
@@ -79,6 +95,6 @@ export class Warikan010Service {
       version: version,
     };
 
-    return this.http.post(this.url_update, body, httpOptions);
+    return this.http.post(this.url_update, body, this.httpOptions);
   }
 }
